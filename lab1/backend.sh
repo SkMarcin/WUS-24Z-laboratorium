a8d167fefc03d61a09b150076f735dec86d1b291
msikorsk <01178592@pw.edu.pl>
2024-11-11 13:35:58 +0100

    Added script to run on backend vm

---
+++ lab1/backend.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+backend_port=$1
+db_ip=$2
+db_port=$3
+
+cd /root
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y default-jre
+echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> ~/.bashrc
+echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+
+git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
+cd spring-petclinic-rest
+sed -i "s/localhost/$db_ip/g" src/main/resources/application-mysql.properties
+sed -i "s/3306/$db_port/g" src/main/resources/application-mysql.properties
+sed -i "s/9966/$backend_port/g" src/main/resources/application.properties
+sed -i "s/active=hsqldb/active=mysql/g" src/main/resources/application.properties
+./mvnw spring-boot:run &
