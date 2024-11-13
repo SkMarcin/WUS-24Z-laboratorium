883ff318cd7cddf4e12edef0da86f1a30b1273fe
msikorsk <01178592@pw.edu.pl>
2024-11-13 02:26:48 +0100

    Added load balancer logging (untested)

--- lab1/load_balancer.sh
+++ lab1/load_balancer.sh
@@ -14,6 +14,18 @@ sudo apt install -y nginx
 
 cat << EOF > /etc/nginx/sites-enabled/load_balancer
 
+log_format custom_log '$remote_addr - [$time_local] '
+                          '"$request" '
+                          'received_ip="$server_addr" '
+                          'received_port="$server_port" '
+                          'forwarded_ip="$upstream_addr" '
+                          'status=$status '
+                          'bytes_sent=$body_bytes_sent '
+                          'response_time=$upstream_response_time';
+
+access_log  /var/log/nginx/access.log custom_log;
+error_log  /var/log/nginx/error_log;
+
 upstream backend {
     server $backend1_ip:$backend1_port;
     server $backend2_ip:$backend2_port;
