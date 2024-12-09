73adeb0ae312a3d47848a905f6fd8a92b4cc2caa
msikorsk <01178592@pw.edu.pl>
2024-12-09 18:36:11 +0100

    Added terraform refresh to load outputs

--- lab2/terraform/output.tf
+++ lab2/terraform/output.tf
@@ -1,3 +1,4 @@
 output "ansible_public_ip" {
   value = azurerm_public_ip.public_ip[sort(keys(azurerm_public_ip.public_ip))[1]].ip_address
+  depends_on = [azurerm_public_ip.public_ip] # Ensure dependency
 }
