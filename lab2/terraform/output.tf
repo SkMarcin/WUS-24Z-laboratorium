f64cf4ac6186d976420fc0b9f7dfd6baf72b3d7f
msikorsk <01178592@pw.edu.pl>
2024-12-09 20:33:45 +0100

    Added dependency for ip to be created

--- lab2/terraform/output.tf
+++ lab2/terraform/output.tf
@@ -1,6 +1,10 @@
 data "azurerm_public_ip" "ansible_public_ip" {
   name                  = "frontend_ip"
   resource_group_name   = "config-1-rg"
+
+  depends_on = [
+    azurerm_public_ip.public_ip["frontend_ip"]
+  ]
 }
 
 output "ansible_public_ip" {
